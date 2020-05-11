26cef87651f754a11e2a1f81c3e8b16694b98362
Cathy D'Onofrio <catd825@gmail.com>
2020-05-11 00:57:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,11 @@
 def my_hash_creator(key, value)
-  my_hash_creator = {
     key => value
-  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  read_from_hash = {
+    
+  }
 end
 
 def update_counting_hash(hash, key)
